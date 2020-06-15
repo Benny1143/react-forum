6fb8e718ca9f072ffb75a904824e17367b831a1a
Benny <bennyrobot1143@gmail.com>
2020-06-15 17:36:20 +0800

    add forum square selection

--- src/components/Forum/Forum.js
+++ src/components/Forum/Forum.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import './Forum.scss';
 // import { connect } from 'react-redux';
 
 class Forum extends Component {
@@ -22,23 +23,36 @@ class Forum extends Component {
         const list = ['ENGLISH', 'ENGLISH LIT.', 'HISTORY', 'E. MATH', 'MALAY', 'A. MATH', 'BIOLOGY', 'PHYSICS']
 
         return (
-            <div>
-                <form onSubmit={this.handleSubmit}>
-                    {
-                        list.map(item =>
-                            <div className="form-check" key={item}>
-                                <input
-                                    type="checkbox"
-                                    className="form-check-input"
-                                    id={item}
-                                    onChange={this.onChangeList}
-                                    defaultChecked={false} />
-                                <label className="form-check-label" htmlFor={item}>{item}</label>
-                            </div>
-                        )
-                    }
-                    <input type="submit" value="Next" />
-                </form>
+            <div className="container">
+                <div className="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+                    <div>1 - 2</div>
+
+                    <div>
+                        <h2>Welcome to the SmartGuppy Forum!</h2>
+                        <p>This is the palce where you can reach out to our vibrant community and ask questions.</p>
+                    </div>
+
+                    <form onSubmit={this.handleSubmit}>
+                        <h4>CHOOSE SUBJECTS YOUR'RE INTERESTED IN</h4>
+                        <input type="submit" value="Next" />
+                        <div className="gridContainer">
+                            {
+                                list.map(item =>
+                                    <div key={item} className="imageBox">
+
+                                        <span className="boxText">{item}</span>
+                                        <input
+                                            type="checkbox"
+                                            id={item}
+                                            onChange={this.onChangeList}
+                                            defaultChecked={false} />
+                                        <label htmlFor={item}></label>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </form>
+                </div>
             </div>
         )
     }
