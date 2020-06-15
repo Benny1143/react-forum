101707af4e5529d871e1046fdfc5198536839875
Benny <bennyrobot1143@gmail.com>
2020-06-15 18:16:12 +0800

    add round checkbox

--- src/components/Forum/Forum.js
+++ src/components/Forum/Forum.js
@@ -24,29 +24,36 @@ class Forum extends Component {
 
         return (
             <div className="container">
-                <div className="col-lg-9 col-md-9 col-sm-12 col-xs-12">
+                <div class="row">
                     <div>1 - 2</div>
-
+                </div>
+                <div className="col-lg-9 col-md-9 col-sm-12 col-xs-12">
                     <div>
                         <h2>Welcome to the SmartGuppy Forum!</h2>
                         <p>This is the palce where you can reach out to our vibrant community and ask questions.</p>
                     </div>
 
                     <form onSubmit={this.handleSubmit}>
-                        <h4>CHOOSE SUBJECTS YOUR'RE INTERESTED IN</h4>
-                        <input type="submit" value="Next" />
+                        <div className="subBar">
+                            <h4>CHOOSE SUBJECTS YOUR'RE INTERESTED IN</h4>
+                            <button type="button" className="nextButton" type="submit">NEXT</button>
+                        </div>
+
                         <div className="gridContainer">
                             {
                                 list.map(item =>
                                     <div key={item} className="imageBox">
-
                                         <span className="boxText">{item}</span>
-                                        <input
-                                            type="checkbox"
-                                            id={item}
-                                            onChange={this.onChangeList}
-                                            defaultChecked={false} />
-                                        <label htmlFor={item}></label>
+
+                                        <div class="round">
+                                            <input
+                                                type="checkbox"
+                                                id={item}
+                                                onChange={this.onChangeList}
+                                                defaultChecked={false} />
+                                            <label htmlFor={item}></label>
+                                        </div>
+
                                     </div>
                                 )
                             }
