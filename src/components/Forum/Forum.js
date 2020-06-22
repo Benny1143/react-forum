3baac1775a84bcfd7cfa3178abe468f84e561fe5
Benny <bennyrobot1143@gmail.com>
2020-06-22 17:51:33 +0800

    add cards to forum

--- src/components/Forum/Forum.js
+++ src/components/Forum/Forum.js
@@ -1,10 +1,27 @@
 import React, { Component } from 'react';
+import './Forum.scss';
 
 class Forum extends Component {
     render() {
         return (
-            <div>
-                Forum main page
+            <div className="container mainContainer">
+                <div className="col-lg-2">
+                    <div className="shadow">
+                        Left side
+                    </div>
+                </div>
+                <div className="col-lg-8">
+                    <div style={{ textAlign: "center" }} className="cardlist">
+                        <div className="card">
+                            Main
+                        </div>
+                    </div>
+                </div>
+                <div className="col-lg-2">
+                    <div className="shadow">
+                        Right side
+                    </div>
+                </div>
             </div>
         );
     }
