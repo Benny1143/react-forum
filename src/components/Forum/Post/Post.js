c045c522c0d8b9538a5a31f290539ee1244bb70e
Benny <bennyrobot1143@gmail.com>
2020-06-25 16:16:17 +0800

    add post first card

--- src/components/Forum/Post/Post.js
+++ src/components/Forum/Post/Post.js
@@ -1,10 +1,44 @@
 import React, { Component } from 'react';
+import cx from 'classnames';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { card } from '../Forum.module.scss';
+import styles from './Post.module.scss';
+import UserImg from '../UserImg/UserImg';
 
 class Post extends Component {
+    constructor() {
+        super()
+        this.state = { input: "" }
+        this.handleInput = this.handleInput.bind(this)
+        this.handlePost = this.handlePost.bind(this)
+    }
+
+    handleInput = (event) => this.setState({ input: event.target.value });
+
+    handlePost = () => console.log(this.state.input)
+
     render() {
+        const input = this.state.input;
         return (
             <div>
-                Post
+                <div className={cx(card, styles.post)}>
+                    <div>
+                        <UserImg />
+                        <input
+                            type="text"
+                            placeholder="What's your question or &quot;aha&quot; moment?"
+                            value={input}
+                            onChange={this.handleInput}
+                        />
+                    </div>
+                    <div>
+                        <FontAwesomeIcon icon="tags" />
+                        <button onClick={this.handlePost} disabled={!input}>POST</button>
+                    </div>
+                </div>
+                <div className={card}>
+                    List
+                </div>
             </div>
         )
     }
